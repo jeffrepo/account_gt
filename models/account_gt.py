c7108e405cb09edfa223368fdfbb6a714ee00d19
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2025-12-01 17:02:53 -0600

    Campos faltantes versión actualizada

--- models/account_gt.py
+++ models/account_gt.py
@@ -4,6 +4,19 @@ from odoo.exceptions import ValidationError
 from odoo.exceptions import UserError, AccessError
 import logging
 
+class AccountACcount(models.Model):
+    _inherit = "account.account"
+
+    combustible = fields.Boolean('Combustible')
+    retencion_iva = fields.Boolean('Iva retencion')
+    uso = fields.Selection([('exento','Exento'),('compra_bien','Compra / bien'),('impuesto_petroleo','Impuesto de petroleo'),('combustible','Combustible'),('retencion_iva','Retencion IVA'),('iva','IVA')],'Uso')
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    columna_farmacia_exento_ventas = fields.Boolean('Columna farmacia exento')
+    gastos_no_deducibles = fields.Boolean('Mostrar gastos no deducibles Libro compras')
+
 class Liquidacion(models.Model):
     _name = "account_gt.liquidacion"
     _inherit = ['mail.thread', 'mail.activity.mixin']
